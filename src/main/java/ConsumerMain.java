cc3caa892f083e9ed3555083df08575ccd9b57a0
Robert <rob.panescu.com>
2024-01-27 16:46:37 +0200

    first commit

---
+++ src/main/java/ConsumerMain.java
@@ -0,0 +1,11 @@
+public class ConsumerMain {
+
+    private static final String QUEUE_NAME = "MyFirstActiveMQ";
+
+    public static void main(String[] args) throws Exception {
+        SimpleQueue queue = new SimpleQueue(QUEUE_NAME);
+        queue.receive();
+        queue.close();
+    }
+
+}
---
+++ src/main/java/ProducerMain.java
@@ -0,0 +1,10 @@
+public class ProducerMain {
+
+    private static final String QUEUE_NAME = "MyFirstActiveMQ";
+
+    public static void main(String[] args) throws Exception {
+        SimpleQueue queue = new SimpleQueue(QUEUE_NAME);
+        queue.send("Welcome to the World of ActiveMQ. Hope you will enjoy this tutorial.");
+        queue.close();
+    }
+}
